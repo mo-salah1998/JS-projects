158de31f5d3b85f25cb04804b39ae5fed6308199
naija963@gmail.com <naija963@gmail.com>
2023-07-17 22:05:17 +0100

    project03:adding a film api to the node project and using distraction to filter some data part 2

--- project03/index.js
+++ project03/index.js
@@ -1,7 +1,37 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://salah:salah123@cluster0.quo6z.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+run().catch(console.dir);
 app.use(express.json());
+
+
+
+
+
 app.use("/", async (req, res) => {
 
     const axios = require('axios');
