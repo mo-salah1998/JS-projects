181e6273c0399f3b5e9e7953c974e178917f479b
naija963@gmail.com <naija963@gmail.com>
2023-08-28 17:24:45 +0100

    stage  dev : backend integrate cv extractor information api

--- project03/routes/index.js
+++ project03/routes/index.js
@@ -1,13 +1,40 @@
 var express = require('express');
 var router = express.Router();
+const axios = require('axios');
+const mindee = require("mindee");
 
+const multer = require('multer');
+
+const storage = multer.memoryStorage(); // Store the file in memory as a Buffer
+const upload = multer({ storage: storage });
 /* GET home page. */
 router.get('/cv', function(req, res, next) {
 
 });
-router.post('/cv', function(req, res, next) {
-    console.log(req);
-    res.send(200).json({message:"ok"});
+router.post('/cv', upload.single('file'),function(req, res, next) {
+    //console.log(req.body);
+    if (!req.file) {
+        return res.status(400).json({ message: 'No file uploaded' });
+    }
+
+    // Access the uploaded file via req.file
+    const uploadedFile = req.file;
+    console.log(uploadedFile);
+    const mindeeClient = new mindee.Client({ apiKey: "80eb38587a94e3f58e2d838eaf6639d3" });
+    const inputSource = mindeeClient.docFromPath("/home/mo-salah/Downloads/Mohamed Salah Naija (1) (1).pdf");
+
+   const apiResponse = mindeeClient.parse(
+        mindee.product.fr.CarteVitaleV1,
+        inputSource
+    );
+    apiResponse.then((resp) => {
+        // print a string summary
+        console.log(resp.document.toString());
+    });
+
+
+    res.status(200).json({ message: "ok" });
+
 });
 router.patch('/cv', function(req, res, next) {
 
--- stage-front/src/components/login.jsx
+++ stage-front/src/components/login.jsx
@@ -69,9 +69,9 @@ export default function LogForm() {
                         </form>
 
                         <p className="mt-10 text-center text-sm text-gray-500">
-                            Not a member?{' '}
+                            Or{' '}
                             <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-                                Start a 14 day free trial
+
                             </a>
                         </p>
                     </div>
